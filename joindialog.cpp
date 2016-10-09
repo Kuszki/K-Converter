49b1dca6b8de98add6a092ecc7d93f42dc7d203c
Kuszki <l.drozdz@o2.pl>
2016-10-09 14:31:48 +0200

    Implement joining UI

--- joindialog.cpp
+++ joindialog.cpp
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                         *
- *  {description}                                                          *
+ *  Tango file converter for DaroD                                         *
  *  Copyright (C) 2016  Łukasz "Kuszki" Dróżdż  l.drozdz@openmailbox.org   *
  *                                                                         *
  *  This program is free software: you can redistribute it and/or modify   *
@@ -17,17 +17,98 @@
  *  along with this program. If not, see http://www.gnu.org/licenses/.     *
  *                                                                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #include "joindialog.hpp"
 #include "ui_joindialog.h"
 
-JoinDialog::JoinDialog(QWidget *parent) :
-	QDialog(parent),
-	ui(new Ui::JoinDialog)
+JoinDialog::JoinDialog(QWidget* Parent)
+: QDialog(Parent), ui(new Ui::JoinDialog)
 {
 	ui->setupUi(this);
+
+	ui->progressBar->setVisible(false);
+
+	connect(ui->Class, &QComboBox::currentTextChanged, this, &JoinDialog::onListRequest);
 }
 
-JoinDialog::~JoinDialog()
+JoinDialog::~JoinDialog(void)
 {
 	delete ui;
+}
+
+void JoinDialog::open(void)
+{
+	if (!ui->Values->count()) emit onListRequest(ui->Class->currentText());
+
+	connect(AppCore::getInstance(), &AppCore::onProgressInit, ui->progressBar, &QProgressBar::setRange);
+	connect(AppCore::getInstance(), &AppCore::onProgressUpdate, ui->progressBar, &QProgressBar::setValue);
+
+	QDialog::open();
+}
+
+void JoinDialog::accept(void)
+{
+	if (ui->Class->currentText().size())
+	{
+		ui->procedButton->setEnabled(false);
+		ui->cancelButton->setEnabled(false);
+		ui->progressBar->setVisible(true);
+
+		QList<int> Values;
+
+		for (int i = 0; i < ui->Values->count(); ++i)
+		{
+			if (ui->Values->item(i)->checkState() == Qt::Checked)
+			{
+				Values.append(i);
+			}
+		}
+
+		emit onJoinRequest(ui->Class->currentText(), Values, ui->Keep->isChecked());
+	}
+	else QMessageBox::warning(this, tr("Error"), tr("Classes list is empty"));
+}
+
+void JoinDialog::reject(void)
+{
+	if (ui->cancelButton->isEnabled())
+	{
+		QDialog::reject();
+
+		disconnect(AppCore::getInstance(), &AppCore::onProgressInit, ui->progressBar, &QProgressBar::setRange);
+		disconnect(AppCore::getInstance(), &AppCore::onProgressUpdate, ui->progressBar, &QProgressBar::setValue);
+
+		if (Joined) { emit onRefreshRequest(); Joined = 0; }
+	}
+	else QMessageBox::warning(this, tr("Error"), tr("Work in progress"));
+}
+
+void JoinDialog::UpdateFields(const QStringList& Fields)
+{
+	ui->Values->clear();
+	ui->Values->addItems(Fields);
+
+	for (int i = 0; i < ui->Values->count(); i++)
+	{
+		ui->Values->item(i)->setFlags(Qt::ItemIsUserCheckable | Qt::ItemIsEnabled | Qt::ItemIsSelectable);
+		ui->Values->item(i)->setCheckState(Qt::Unchecked);
+	}
+}
+
+void JoinDialog::UpdateList(const QStringList& Classes)
+{
+	ui->Class->clear();
+	ui->Class->addItems(Classes);
+}
+
+void JoinDialog::ShowProgress(int Count)
+{
+	QMessageBox::information(this, tr("Join progress"),
+						tr("Joined %n item(s)", 0, Count));
+
+	ui->procedButton->setEnabled(true);
+	ui->cancelButton->setEnabled(true);
+	ui->progressBar->setVisible(false);
+
+	Joined += Count;
 }
--- joindialog.hpp
+++ joindialog.hpp
@@ -1,6 +1,6 @@
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *                                                                         *
- *  {description}                                                          *
+ *  Tango file converter for DaroD                                         *
  *  Copyright (C) 2016  Łukasz "Kuszki" Dróżdż  l.drozdz@openmailbox.org   *
  *                                                                         *
  *  This program is free software: you can redistribute it and/or modify   *
@@ -17,25 +17,56 @@
  *  along with this program. If not, see http://www.gnu.org/licenses/.     *
  *                                                                         *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
 #ifndef JOINDIALOG_HPP
 #define JOINDIALOG_HPP
 
+#include <QMessageBox>
 #include <QDialog>
 
-namespace Ui {
+#include "appcore.hpp"
+
+namespace Ui
+{
 	class JoinDialog;
 }
 
 class JoinDialog : public QDialog
 {
+
 		Q_OBJECT
 
-	public:
-		explicit JoinDialog(QWidget *parent = 0);
-		~JoinDialog();
-
 	private:
+
 		Ui::JoinDialog *ui;
+
+		int Joined = 0;
+
+	public:
+
+		explicit JoinDialog(QWidget* Parent = nullptr);
+		virtual ~JoinDialog(void) override;
+
+	public slots:
+
+		virtual void open(void) override;
+		virtual void accept(void) override;
+		virtual void reject(void) override;
+
+		void UpdateFields(const QStringList& Fields);
+		void UpdateList(const QStringList& Classes);
+
+		void ShowProgress(int Count);
+
+	signals:
+
+		void onJoinRequest(const QString&,
+					    const QList<int>&,
+					    bool);
+		void onRefreshRequest(void);
+
+		void onListRequest(const QString&);
+
 };
 
 #endif // JOINDIALOG_HPP
